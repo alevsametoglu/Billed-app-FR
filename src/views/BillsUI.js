6c1a3e62430eff85958dcf8957e4ec69a5be1393
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-12-13 23:10:28 +0100

    fix-bug the bills ordered from earliest to latest

--- src/views/BillsUI.js
+++ src/views/BillsUI.js
@@ -1,31 +1,32 @@
 import VerticalLayout from './VerticalLayout.js'
-import ErrorPage from "./ErrorPage.js"
-import LoadingPage from "./LoadingPage.js"
+import ErrorPage from './ErrorPage.js'
+import LoadingPage from './LoadingPage.js'
 
 import Actions from './Actions.js'
+import { formatDate } from '../app/format.js'
 
 const row = (bill) => {
-  return (`
+  return `
     <tr>
       <td>${bill.type}</td>
       <td>${bill.name}</td>
-      <td>${bill.date}</td>
+      <td data-testid='bill-date' value='${bill.date}'>${formatDate(bill.date)}</td>
       <td>${bill.amount} €</td>
       <td>${bill.status}</td>
       <td>
         ${Actions(bill.fileUrl)}
       </td>
     </tr>
-    `)
-  }
+    `
+}
 
 const rows = (data) => {
-  return (data && data.length) ? data.map(bill => row(bill)).join("") : ""
+  return data && data.length ? data.map((bill) => row(bill)).join('') : ''
 }
 
 export default ({ data: bills, loading, error }) => {
-  
-  const modal = () => (`
+  bills = bills ? bills.sort((a, b) => (a.date > b.date ? -1 : 1)) : []
+  const modal = () => `
     <div class="modal fade" id="modaleFile" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
         <div class="modal-content">
@@ -40,15 +41,15 @@ export default ({ data: bills, loading, error }) => {
         </div>
       </div>
     </div>
-  `)
+  `
 
   if (loading) {
     return LoadingPage()
   } else if (error) {
     return ErrorPage(error)
   }
-  
-  return (`
+
+  return `
     <div class='layout'>
       ${VerticalLayout(120)}
       <div class='content'>
@@ -76,5 +77,4 @@ export default ({ data: bills, loading, error }) => {
       </div>
       ${modal()}
     </div>`
-  )
 }
