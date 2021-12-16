66160469115102ed2fd94a7817a946851353b580
alev sametoglu <alevsametoglu.fr@gmail.com>
2021-12-16 13:40:23 +0100

    fixed test unitaire and integration

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,17 +2,35 @@ import { fireEvent, screen } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import Bills from "../containers/Bills.js"
-
-import { ROUTES } from "../constants/routes.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
 import firebase from "../__mocks__/firebase.js"
+import Firestore from "../app/Firestore.js"
+import Router from "../app/Router"
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: [] })
-      document.body.innerHTML = html
-      //to-do write expect expression
+      // const html = BillsUI({ data: [bills] })
+      // document.body.innerHTML = html
+      // //to-do write expect expression
+      jest.mock("../app/Firestore")
+      Firestore.bills = () => ({ bills, get: jest.fn().mockResolvedValue() })
+
+      window.localStorage.setItem(
+        "user",
+        JSON.stringify({
+          email: "test@test.com",
+          type: "Employee",
+        }),
+      )
+      const pathname = ROUTES_PATH["Bills"]
+      Object.defineProperty(window, "location", { value: { hash: pathname } })
+      document.body.innerHTML = `<div id="root"></div>`
+      Router()
+      expect(screen.getByTestId("icon-window").classList).toContain("active-icon")
     })
+
     test("Then bills should be ordered from earliest to latest", () => {
       const html = BillsUI({ data: bills })
       document.body.innerHTML = html
@@ -49,6 +67,7 @@ describe("When I click on button 'Nouvelle note de frais'", () => {
     const onNavigate = (pathname) => {
       document.body.innerHTML = ROUTES({ pathname })
     }
+
     const bill = new Bills({
       document,
       onNavigate,
